dd76ad2426e10b80f0dbec128fe090d5e6f5ba58
Ravi Joshi <rrjoshi92@gmail.com>
2021-11-09 01:45:59 -0800

    form component animation added.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import './App.css';
-import {useState} from 'react'
+import { useState } from 'react'
 import Header from './Components/Header'
 import Home from './Components/Home'
 import Spinner from './Components/Spinner'
-
+import Form from './Components/Form'
+import { CSSTransition } from 'react-transition-group'
 
 function App() {
   const [loading, setloading] = useState(true);
+  const [showForm, setShowForm] = useState(false);
 
   setTimeout(() => {
     setloading(false)
-  }, 2000);
+  }, 1000);
+
   return (
     <div className="App">
       <Header></Header>
       {!loading ?
         <>
-          <Home/>
+          <Home toggleForm={setShowForm}></Home>
+          <section>
+            <CSSTransition in={showForm} unmountOnExit timeout={300} classNames="form">
+              <Form showForm={showForm} toggleForm={setShowForm} />
+            </CSSTransition>
+          </section>
         </>
-        : <><Spinner/></>
+        : <><Spinner /></>
       }
     </div>
   );
