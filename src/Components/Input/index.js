6b45e37ff62d2f1d2ad82ef948f049de4750450f
Ravi Joshi <rrjoshi92@gmail.com>
2021-11-15 23:44:51 -0800

    Input component added.

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,9 +1,9 @@
 import { Wrapper } from './Input.styles'
 
-export default function Input({placeHolder}) {
+export default function Input({ placeHolder, onChange, itemName}) {
     return (
         <Wrapper>
-            <input type="text" placeholder={placeHolder}></input>
+            <input type="text" defaultValue={itemName} onChange={onChange} placeholder={placeHolder}></input>
         </Wrapper>
     )
 }
