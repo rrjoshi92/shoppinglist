8817189de9c6e06fca1b8a8d754e250d0db42d72
Ravi Joshi <rrjoshi92@gmail.com>
2021-11-15 23:48:28 -0800

    App component and global style change.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,114 @@
 import './App.css';
-import { useState } from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { getItems, addItem, deleteItem, updateItem } from './services/firebaseService'
 import Header from './Components/Header'
 import Home from './Components/Home'
 import Spinner from './Components/Spinner'
-import Form from './Components/Form'
-import { CSSTransition } from 'react-transition-group'
+import List from './Components/List'
+
 
 function App() {
-  const [loading, setloading] = useState(true);
-  const [showForm, setShowForm] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-  setTimeout(() => {
-    setloading(false)
-  }, 1000);
+  useEffect(() => {
+    getItems((data) => {
+      setLoading(true);
+      setData(data);
+      setLoading(false);
+    });
+  }, []);
+
+  const [renderList, setRenderList] = useState([]);
+
+  useEffect(() => {
+    let itemList = [];
+    if (data) {
+      for (let item of data) {
+        for (let i = 0; i < item.count; i++) {
+          itemList.push({
+            renderID: `${item.id}_${i}`,
+            ...item
+          })
+        }
+      }
+    }
+    setRenderList(itemList);
+  }, [data]);
+
+
+  const onPurchase = (e) => {
+
+    const updatedRenderList = renderList.map((item) => {
+      if (item.renderID === e.currentTarget.getAttribute('renderid')) {
+        let updatedItem;
+        if (e.currentTarget.checked) {
+          updatedItem = {
+            ...item,
+            purchased: true,
+          };
+        } else {
+          updatedItem = {
+            ...item,
+            purchased: false,
+          };
+        }
+        return updatedItem;
+      }
+      return item;
+    });
+    setRenderList(updatedRenderList);
+  }
+
+  const onUpdateItem = ({ name, info, count, purchased, renderID }) => {
+    if (name && info && count) {
+      if (renderID !== undefined) {
+        let id = renderID.split('_')[0];
+        updateItem(id, {
+          name,
+          info,
+          count,
+          purchased
+        });
+      } else {
+        addItem({
+          name,
+          info,
+          count,
+          purchased: false
+        });
+      }
+    }
+  }
+
+  const onDelete = ({ renderID }) => {
+    let id = renderID.split('_')[0];
+    deleteItem(id);
+  }
+
+
+  const ShoppingList = () => {
+    return (
+      <List
+        itemList={renderList}
+        onPurchase={onPurchase}
+        onUpdate={onUpdateItem}
+        onDelete={onDelete}
+      />
+    )
+  }
+
 
   return (
-    <div className="App">
+    <>
       <Header></Header>
-      {!loading ?
-        <>
-          <Home toggleForm={setShowForm}></Home>
-          <section>
-            <CSSTransition in={showForm} unmountOnExit timeout={300} classNames="form">
-              <Form showForm={showForm} toggleForm={setShowForm} />
-            </CSSTransition>
-          </section>
-        </>
-        : <><Spinner /></>
+      {
+        loading ? <Spinner /> : <Routes>
+          <Route path="/" exact element={data && Object.keys(data).length > 0 ? ShoppingList() : <Home onUpdate={onUpdateItem} />} />
+        </Routes>
       }
-    </div>
+    </>
   );
 }
 
