2c340ba90914cb0f0118d900de036230e59180c7
Ravi Joshi <rrjoshi92@gmail.com>
2021-11-15 23:44:07 -0800

    Textarea component added.

--- src/Components/Textarea/index.js
+++ src/Components/Textarea/index.js
@@ -3,17 +3,18 @@ import { Wrapper } from './Textarea.styles'
 
 
 
-export default function TextArea({ placeHolder, maxlength }) {
+export default function TextArea({ placeHolder, maxlength, itemInfo, onChange }) {
 
-    const [charCount, setcharCount] = useState(0);
+    const [charCount, setcharCount] = useState(itemInfo ? itemInfo.length : 0);
 
     const onChangeHandler = (event) =>{
         setcharCount(event.target.value.length);
+        onChange(event);
     }
 
     return (
         <Wrapper>
-            <textarea placeholder={placeHolder} maxLength={maxlength} onChange={onChangeHandler}>
+            <textarea placeholder={placeHolder} defaultValue={itemInfo} maxLength={maxlength} onChange={onChangeHandler}>
             </textarea>
             <p>{charCount}/{maxlength}</p>
         </Wrapper>
